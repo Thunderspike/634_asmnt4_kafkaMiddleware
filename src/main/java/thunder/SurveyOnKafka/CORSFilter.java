7716dda3752cd4d10dfa4ba2817682c4ee582461
Pol Ajazi <pol.ajazi@yahoo.com>
2021-05-05 07:14:56 -0400

    revertinf changes

--- src/main/java/thunder/SurveyOnKafka/CORSFilter.java
+++ src/main/java/thunder/SurveyOnKafka/CORSFilter.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
-    public void filter(
-        ContainerRequestContext requestContext,
-        ContainerResponseContext responseContext
-    ) throws IOException {
-        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-        headers.add("Access-Control-Allow-Origin", "*"); //Allow Access from everywhere   
-        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");            
-        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    @Override
+    public void filter(ContainerRequestContext request,
+            ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers",
+                "CSRF-Token, X-Requested-By, Authorization, Content-Type");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
